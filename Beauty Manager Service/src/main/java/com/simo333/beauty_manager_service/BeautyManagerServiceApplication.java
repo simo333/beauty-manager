8529efaac2ac3152f88f0a60a534cf22eb6a2c0a
simo333 <szymon.333@wp.pl>
2022-09-14 18:52:40 +0200

    Initial commit

---
+++ Beauty Manager Service/src/main/java/com/simo333/beauty_manager_service/BeautyManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simo333.beauty_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyManagerServiceApplication.class, args);
+    }
+
+}
